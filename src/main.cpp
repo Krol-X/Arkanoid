813befe552790ad8ba24e128d9d3dab6a3e36996
Alex Krol <krolmail@list.ru>
2024-04-22 16:11:48 +0300

    Fix windows subsystem:console support

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,44 @@
 #include "game.h"
 
-#ifdef _WIN32
-#include <windows.h>
-#endif
-
-#ifdef _WIN32
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-#else
-int main(int argc, char** argv)
-#endif
+int _main(int argc, char** argv)
 {
     Game game;
     game.Run();
 
     return 0;
+}
+
+#ifdef _WIN32
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    int argc;
+    wchar_t** argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+    char** utf8_argv = new char* [argc];
+
+    for (int i = 0; i < argc; ++i)
+    {
+        int size_needed = WideCharToMultiByte(CP_UTF8, 0, argv[i], -1, NULL, 0, NULL, NULL);
+        utf8_argv[i] = new char[size_needed];
+        WideCharToMultiByte(CP_UTF8, 0, argv[i], -1, utf8_argv[i], size_needed, NULL, NULL);
+    }
+    LocalFree(argv);
+
+    int result = _main(argc, utf8_argv);
+
+    for (int i = 0; i < argc; ++i)
+    {
+        delete[] utf8_argv[i];
+    }
+    delete[] utf8_argv;
+
+    return result;
+}
+#endif
+
+int main(int argc, char** argv)
+
+{
+    return _main(argc, argv);
 }
