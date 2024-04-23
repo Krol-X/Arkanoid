142951a25c11c6d2d2fd6a1a3d45a0f2b1aac3ed
Alex Krol <krolmail@list.ru>
2024-04-23 16:05:42 +0300

    Add upx compression to Arkanoid.exe

--- installer/install.cmd
+++ installer/install.cmd
@@ -4,6 +4,7 @@ cd ..\Release
 del *.ini
 del *.lib
 del game.exe
+..\installer\upx.exe Arkanoid.exe
 ..\installer\7zr.exe a archive.7z *.* -mx5 -mf=BCJ2
 copy /b ..\installer\7zS2.sfx + ..\installer\config.txt + archive.7z game.exe
 rem ..\installer\upx.exe game.exe
